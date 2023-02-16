301f5d0783d13f5de54f0ea291f720ad36bc4384
CyberShriver <irakozep18.pi@gmail.com>
2023-02-16 21:53:59 +0200

    connect modal to navbar to be opened

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,24 +1,85 @@
 <template>
-    <header class=" bg-weather-primary shadow-lg sticky top-0">
+  <header class="bg-weather-primary shadow-lg sticky top-0">
+    <nav
+      class="
+        container
+        flex flex-col
+        text-white
+        items-center
+        gap-4
+        py-6
+        sm:flex-row
+      "
+    >
+      <RouterLink :to="{ name: 'Home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <div class="text-2xl">The Local Weather</div>
+        </div>
+      </RouterLink>
 
-        <nav class="container flex flex-col text-white items-center gap-4 py-6 sm:flex-row ">
-            <RouterLink :to="{name:'Home'}">
-                <div class="flex  items-center gap-3">
-                    <i class="fa-solid fa-sun text-2xl"></i>
-                    <div class="text-2xl">The Local Weather</div>
-                </div>
-            </RouterLink>
+      <div class="flex items-center flex-1 justify-end gap-3">
+        <i
+          class="
+            fa-solid fa-circle-info
+            text-xl
+            hover:text-weather-secondary
+            duration-150
+            cursor-pointer
+          "
+          @click="toggleModal"
+        ></i>
+        <i
+          class="
+            fa-solid fa-plus
+            text-xl
+            hover:text-weather-secondary
+            duration-150
+            cursor-pointer
+          "
+        ></i>
+      </div>
 
-            <div class="flex items-center flex-1 justify-end gap-3">
-                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
-                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
-            </div>
+      <GlobalModal :modalActive="modalActive" @close-modal="toggleModal">
+        <div class="text-black">
+          <h1 class="text-2xl mb-1">About:</h1>
+          <p class="mb-4">
+            The Local Weather allows you to track the current and future weather
+            of cities of your choosing.
+          </p>
+          <h2 class="text-2xl">How it works:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+              Search for your city by entering the name into the search bar.
+            </li>
+            <li>
+              Select a city within the results, this will take you to the
+              current weather for your selection.
+            </li>
+            <li>
+              Track the city by clicking on the "+" icon in the top right. This
+              will save the city to view at a later time on the home page.
+            </li>
+          </ol>
 
-        </nav>
-
-    </header>
+          <h2 class="text-2xl">Removing a city</h2>
+          <p>
+            If you no longer wish to track a city, simply select the city within
+            the home page. At the bottom of the page, there will be am option to
+            delete the city.
+          </p>
+        </div>
+      </GlobalModal>
+    </nav>
+  </header>
 </template>
 
 <script setup>
+import { ref } from "@vue/reactivity";
+import GlobalModal from "./GlobalModal.vue";
 
+const modalActive = ref(null);
+const toggleModal = () => {
+  modalActive.value = !modalActive.value;
+};
 </script>
