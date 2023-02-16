3674d889f7a35f72e1d2db33ef7a730ba3d69436
CyberShriver <irakozep18.pi@gmail.com>
2023-02-16 20:41:47 +0200

    create first components 'navbar'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-
+  <div class="flex flex-col bg-weather-primary min-h-screen font-Roboto ">
+    <navbar/>
+  </div>
 </template>
 
 <script setup>
+import navbar from './components/navbar.vue';
 
 </script>
