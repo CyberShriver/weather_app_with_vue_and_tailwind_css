59b23fff3bec2f9d8cc79a01ed60a98c86150ada
CyberShriver <irakozep18.pi@gmail.com>
2023-02-16 20:39:18 +0200

    in weather project with vue and tailwind css

---
+++ src/components/navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class=" bg-weather-primary shadow-lg sticky top-0">
+
+        <nav class="container flex flex-col text-white items-center gap-4 py-6 sm:flex-row ">
+            <RouterLink :to="{name:'Home'}">
+                <div class="flex  items-center gap-3">
+                    <i class="fa-solid fa-sun text-2xl"></i>
+                    <div class="text-2xl">The Local Weather</div>
+                </div>
+            </RouterLink>
+
+            <div class="flex items-center flex-1 justify-end gap-3">
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+            </div>
+
+        </nav>
+
+    </header>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+   {   
+     path:"/",
+    name:"Home",
+    component:HomeView
+  }
+
+  ]
+})
+
+export default router
